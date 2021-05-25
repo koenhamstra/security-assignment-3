db338057dc0d8a312bb6a82be4a66e47fa6cc3cd
Koen Hamstra <koen@Koens-MacBook-Pro.local>
2021-05-25 15:30:24 +0200

    create login info

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,5 +16,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([BlogSeeder::class]);
+
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('Welcome123'),
+        ]);
     }
 }
